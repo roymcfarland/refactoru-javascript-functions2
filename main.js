9e74c44d635c6ab7d218f3e097b9857505480359
Roy McFarland <roy.mcfarland@gmail.com>
2014-09-24 17:33:35 -0600

    a show improved

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-// USING UNDERSCORE.JS //
-
-/////////////////////////////////////
-/////////// EXERCISE 1 //////////////
-/////////////////////////////////////
-
-// var originalObj = [{
-// 	name: 'Luisa',
-// 	age: 25
-// }];
-// var getName = _.pluck(originalObj, 'name');
-// console.log(getName);
-
-
-
-
-// USING JQUERY //
-
-$(document).on('ready', function() {
-
 /////////////////////////////////////
 /////////// EXERCISE 1 //////////////
 /////////////////////////////////////
@@ -61,6 +41,60 @@ $(document).on('ready', function() {
 /////////// EXERCISE 4 //////////////
 /////////////////////////////////////
 
+// var negativeIndex = function(array, num) {
+// 	var newIndex = (num * -1) - 1;
+// 	var newArray = array.reverse();
+// 	return newArray[newIndex];
+// };
+// console.log( negativeIndex(['a', 'b', 'c', 'd', 'e'], -2) );
+// console.log( negativeIndex(['jerry', 'sarah', 'sally'], -1) );
+
+
+
+/////////////////////////////////////
+/////////// EXERCISE 5 //////////////
+/////////////////////////////////////
+
+// var removeM = function(word) {
+// 	return word.replace('m', '');
+// };
+// console.log( removeM('family') );
+// console.log( removeM('memory') );
+
+
+
+/////////////////////////////////////
+/////////// EXERCISE 6 //////////////
+/////////////////////////////////////
+
+// var printObject = function(obj) {
+// 	for (var key in obj) {
+// 		console.log(key + ' is ' + obj[key]);
+// 	}
+// };
+// printObject({ a: 10, b: 20, c: 30 });
+// printObject({ firstName: 'pork', lastName: 'chops' });
+
+
+
+/////////////////////////////////////
+/////////// EXERCISE 7 //////////////
+/////////////////////////////////////
+
+var vowels = function(string) {
+	string.split('').filter(function(letter){
+		if (letter === 'a' || letter === 'e' || letter === 'i' || letter === 'o' || letter === 'u'){
+			return true} else {
+			return false}
+			}
+console.log( vowels('alabama') );
+console.log( vowels('What lets in youth?') );
+
+
+
+/////////////////////////////////////
+/////////// EXERCISE 8 //////////////
+/////////////////////////////////////
 
 
 
@@ -79,11 +113,3 @@ $(document).on('ready', function() {
 
 
 
-
-
-
-
-
-
-
-});
